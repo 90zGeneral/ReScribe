ece1f6cbacb1c0327255d982b47d40c33547454a
Roydon Jeffrey <brushdam14823@gmail.com>
2017-01-26 21:17:42 -0500

    Ask the user's permission to access their audio recordings and if granted, then create an AVAudioPlayer that grabs the url path to the audio recording and play the sound. Then, set up a speech recognizer to convert the audio into text.

--- ReScribe/ViewController.swift
+++ ReScribe/ViewController.swift
@@ -7,19 +7,52 @@
 //
 
 import UIKit
+import Speech
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    //Outlets
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
+    @IBOutlet weak var TranscribeTextField: UITextView!
+    
+    //Declare an audio player
+    var audioPlayer: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        spinner.isHidden = true
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //To get the user's authorization to record and/or use their voice recordings
+    func speechAuthRequest() {
+        SFSpeechRecognizer.requestAuthorization { (authStatus) in
+            
+            //Check if the user authorized the access
+            if authStatus == SFSpeechRecognizerAuthorizationStatus.authorized {
+                
+                //Find the audio using its path with error handling
+                if let path = Bundle.main.url(forResource: "test", withExtension: "m4a") {
+                    do {
+                        let sound = try AVAudioPlayer(contentsOf: path)
+                        self.audioPlayer = sound
+                        sound.play()
+                    }catch {
+                        print(error.localizedDescription)
+                    }
+                    
+                    //To analyze the speech in the audio
+                    let recognizer = SFSpeechRecognizer()
+                    let request = SFSpeechURLRecognitionRequest(url: path)
+                    recognizer?.recognitionTask(with: request) { (result, error) in
+                        if let error = error {
+                            print("Sorry, we've got a problem here: \(error)")
+                        }
+                    }
+                }
+            }
+        }
     }
-
-
 }
 
